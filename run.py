daee4b7a538853e824aa61b630dc898b39751b17
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-10-14 18:29:43 -0300

    already works

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 from project import app
 
 if __name__ == '__main__':
-    app
+    port = int(os.environ['PORT'])
+    app.run(port=port)
