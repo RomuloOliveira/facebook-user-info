0c7c0cce3f3bb8529da262f45a6e223f9b3dab9d
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-10-14 18:56:18 -0300

    first test working

--- tests/api/test_person.py
+++ tests/api/test_person.py
@@ -15,3 +15,19 @@ class PersonTest(unittest.TestCase):
     def test_index(self):
         res = self.client.get('/')
         self.assertEquals(res.status_code, 200)
+
+    def test_add(self):
+        data = {
+            'facebook_id': '123456'
+        }
+
+        res = self.client.post('/person', data=data)
+        self.assertEquals(res.status_code, 201)
+
+    def test_try_add_not_valid(self):
+        data = {
+            'wrong_parameter': '123456'
+        }
+
+        res = self.client.post('/person', data=data)
+        self.assertEquals(res.status_code, 400)
